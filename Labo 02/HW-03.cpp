187ca482817cdf8317ae8477894ea1ae41a4df40
Chris Beltrán <chrisgonza0@gmail.com>
2020-01-15 20:34:18 -0600

    Add: homework lab 02

--- Labo 02/HW-03.cpp
+++ Labo 02/HW-03.cpp
@@ -1,4 +1,6 @@
+//
 #include <iostream>
+//
 #include <string>
 using namespace std;
 
@@ -10,15 +12,72 @@ struct person{
 	float monthlySalary;	
 };
 
-float proccess(float monthlySalary);
+int salary(int size);
+float proccess(int year, float salary);
 
 //Main funtion
 int main(){
+	person total;
+	int size = 0;
 	
-	proccess(DUI);
-	proccess(name);
+	//Printinfg on screen
+	cout << "Determine a cuantos empleados ingresara al sistema: ";
+	cin >> size;
+	cin.ignore();
 	
+	//Initializing function 
+	salary(size);
+	return 0;
+}
+
+//Personal information function
+int salary(int customers){
+	person total;
 	
+	//Pointer
+	int *array;
+	
+	//Creating new memory spaces
+	array = new int[customers];
+	
+	//Running FOR cycle personal information
+	for (int i = 0; i < customers; i++){
+		cout << "Ingrese el nombre del empleado/a " << (i + 1) << ":\t";
+		getline (cin, total.name);
+		cout << "Ingrese el numero de DUI:\t";
+		cin >> total.DUI;
+		cout << "Ingrese el anio en el que fue contratado:\t";
+		cin >> total.hired;
+		cout << "Ingrese el salario mensual que recibe: $";
+		cin >> total.monthlySalary;
+		cin.ignore();
+		
+		//Initializing function
+		proccess(total.hired, total.monthlySalary);
+		
+	}
+	
+	return 0;	
+}
+
+//Salary estimation function
+float proccess(int year, float salary){
+	float estimation = 0;
+	int present = 0;
+	int months = 0;
+	
+	//Subtraction of the present year with the year when the employee was hired 
+	present = (2020 - year);
+	
+	//Multiplication of total of years working with number of the months of the year
+	months = (12 * present);
+	
+	//Multiplication of total of months worked with the salary per month
+	estimation = (months * salary);
+	
+	//Printing
+	cout << "La estimacion del salario devengado en todos los anios que lleva trabajando es de $: " << endl;
+	cout << estimation << endl;
 	
 	return 0;
 }
